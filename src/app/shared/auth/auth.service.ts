17d20994a311b5ccfa334f38a9e9c159cb70ef4c
Kalon Ohmstede <kalonohmstede@gmail.com>
2022-11-20 00:48:59 -0600

    minor changes and corrections to code

--- src/app/shared/auth/auth.service.ts
+++ src/app/shared/auth/auth.service.ts
@@ -23,62 +23,62 @@ export interface AuthResponseData {
 }
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
   // apiKey = "AIzaSyAgf7dwf83h2itaU0ogPPIy-12CzH1ceRo";
   signupUrl = `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${environment.firebaseAPIKey}`;
   loginUrl = `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${environment.firebaseAPIKey}`;
 
-  currentUser = new BehaviorSubject<User | null>
-  (null);
-  userToken: string = "";
+  currentUser = new BehaviorSubject<User | null>(null);
+  userToken: string = '';
 
   private tokenExpireTimer: any;
 
-  constructor(
-    private http: HttpClient,
-    private router: Router,
-
-  ) { }
+  constructor(private http: HttpClient, private router: Router) {}
 
   signup(email: string, password: string) {
-    return this.http.post<AuthResponseData>(this.signupUrl, {
-      email,
-      password,
-      returnSecureToken: true,
-    })
-    .pipe(
-      tap((res) => {
-      const { email, localId, idToken, expiresIn } = res;
-      this.handleAuth(email, localId, idToken, Number(expiresIn));
-    })
-    );
+    return this.http
+      .post<AuthResponseData>(this.signupUrl, {
+        email,
+        password,
+        returnSecureToken: true,
+      })
+      .pipe(
+        tap((res) => {
+          const { email, localId, idToken, expiresIn } = res;
+          this.handleAuth(email, localId, idToken, Number(expiresIn));
+        })
+      );
   }
 
   login(email: string, password: string) {
-    return this.http.post<AuthResponseData>(this.loginUrl, {
-      email,
-      password,
-      returnSecureToken: true,
-    })
-    .pipe(
-      tap((res) => {
-      const { email, localId, idToken, expiresIn } = res;
-      this.handleAuth(email, localId, idToken, Number(expiresIn));
-    })
-    );
+    return this.http
+      .post<AuthResponseData>(this.loginUrl, {
+        email,
+        password,
+        returnSecureToken: true,
+      })
+      .pipe(
+        tap((res) => {
+          const { email, localId, idToken, expiresIn } = res;
+          this.handleAuth(email, localId, idToken, Number(expiresIn));
+        })
+      );
   }
 
   handleAuth(email: string, userId: string, token: string, expiresIn: number) {
-    const expDate = new Date( new Date().getTime() + expiresIn * 1000);
-    const formUser = new User(email, userId, token, expDate)
+    const expDate = new Date(new Date().getTime() + expiresIn * 1000);
+    const formUser = new User(email, userId, token, expDate);
     this.currentUser.next(formUser);
 
     //serialize user
-    localStorage.setItem(environment.userDataLocalStorageKey, JSON.stringify(formUser));
+    localStorage.setItem(
+      environment.userDataLocalStorageKey,
+      JSON.stringify(formUser)
+    );
 
-    this.autoSignOut(expiresIn * 1000)
+    this.autoSignOut(expiresIn * 1000);
   }
 
   signOut() {
@@ -87,32 +87,33 @@ export class AuthService {
     localStorage.removeItem(environment.userDataLocalStorageKey);
 
     if (this.tokenExpireTimer) clearTimeout(this.tokenExpireTimer);
-    
+
     this.router.navigate(['auth']);
   }
 
   autoSignIn() {
-    console.log("auto sign in")
-    const userData: UserData = JSON.parse(localStorage.getItem(environment.userDataLocalStorageKey) ?? "")
+    console.log('auto sign in');
+    const userData: UserData = JSON.parse(
+      localStorage.getItem(environment.userDataLocalStorageKey) ?? ''
+    );
 
     console.log(userData);
 
     if (!userData) return;
 
-    const {email, id, _token, _tokenExpiration}: UserData = userData;
-    const loadedUser = new User( email, id, _token, new Date(_tokenExpiration));
+    const { email, id, _token, _tokenExpiration }: UserData = userData;
+    const loadedUser = new User(email, id, _token, new Date(_tokenExpiration));
 
     console.log(loadedUser);
 
     if (loadedUser.token) {
       this.currentUser.next(loadedUser);
     }
-    console
+    console.log(this.userToken);
   }
 
   autoSignOut(expireDuration: number) {
-    console.log("Expire Duration: ", expireDuration);
-    this.tokenExpireTimer = setTimeout(this.signOut, expireDuration)
+    console.log('Expire Duration: ', expireDuration);
+    this.tokenExpireTimer = setTimeout(this.signOut, expireDuration);
   }
-
 }
